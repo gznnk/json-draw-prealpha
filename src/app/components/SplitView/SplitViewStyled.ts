0f1238c449eb4156e7395cc7630c48cec35429e3
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-05-16 16:49:54 +0900

    スタイル修正

--- src/app/components/SplitView/SplitViewStyled.ts
+++ src/app/components/SplitView/SplitViewStyled.ts
@@ -16,8 +16,22 @@ export const Pane = styled.div`
 export const Divider = styled.div`
   width: 5px;
   cursor: col-resize;
-  background-color: #3A415C;
-  &:hover {
-    background-color: #666;
+  background-color: transparent;
+  position: relative;
+  
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 2px;
+    width: 1px;
+    height: 100%;
+    background-color: #3A415C;
+  }
+
+  &:hover::after {
+    left: 1px;
+    width: 3px;
+    background-color: #2A3366;
   }
 `;
