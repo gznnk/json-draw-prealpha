651a3c3dd6f1f3cb6175c4166dbea6890d2338f7
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-06-20 16:06:32 +0900

    composeEvenHandlerのHooks化

--- src/features/svg-canvas/utils/common/composeEventHandlers.ts
+++
@@ -1,9 +0,0 @@
-export const composeEventHandlers = <T>(
-	...listeners: Array<(event: T) => void>
-): ((event: T) => void) => {
-	return (event: T) => {
-		for (const listener of listeners) {
-			listener(event);
-		}
-	};
-};
