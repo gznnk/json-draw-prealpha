ba066a6d444f3977c312b6241d5e23cb7831e8fc
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-07-06 23:55:26 +0900

    古い参照のバイパス削除

--- src/features/svg-canvas/components/shapes/ConnectPoint/index.tsx
+++ src/features/svg-canvas/components/shapes/ConnectPoint/index.tsx
@@ -1,7 +1,5 @@
 export { ConnectPoint } from "./ConnectPoint/ConnectPoint";
-export { ConnectPoints } from "./ConnectPoints";
-export { generateOptimalShapeToShapeConnection } from "../../../utils/shapes/connectPoint/generateOptimalShapeToShapeConnection";
-export { getLineDirection } from "../../../utils/shapes/connectPoint/getLineDirection";
 export type { Direction } from "./ConnectPoint/ConnectPointTypes";
+export { ConnectPoints } from "./ConnectPoints";
 export type { ConnectPointsProps } from "./ConnectPoints/ConnectPointsTypes";
 export { PreviewConnectLine } from "./PreviewConnectLine";
