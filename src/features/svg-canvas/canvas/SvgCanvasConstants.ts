0b8ecdeb929360e9664b4aaacc823e1a12e376d3
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-07-12 16:47:28 +0900

    定数調整

--- src/features/svg-canvas/canvas/SvgCanvasConstants.ts
+++ src/features/svg-canvas/canvas/SvgCanvasConstants.ts
@@ -22,7 +22,7 @@ export const AUTO_SCROLL_STEP_SIZE = 15;
 /**
  * Interval duration for continuous auto-scrolling when cursor is at edge (in milliseconds).
  */
-export const AUTO_SCROLL_INTERVAL_MS = 25;
+export const AUTO_SCROLL_INTERVAL_MS = 20;
 
 /**
  * Id of the group for multi-select elements.
