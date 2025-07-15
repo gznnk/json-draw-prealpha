38cc21f5d0ba4277d439d7fc5c9c06d611b0101e
masato nakagawa <m.nakagawa.3625@gmail.com>
2025-07-15 18:47:48 +0900

    コミット漏れ

--- src/features/svg-canvas/hooks/tools/useConnectNode.ts
+++ src/features/svg-canvas/hooks/tools/useConnectNode.ts
@@ -1,1 +1,28 @@
-export const useConnectNode = () => {};
+// Import React.
+import { useCallback } from "react";
+
+// Import types.
+import type { ConnectNodesEvent } from "../../types/events/ConnectNodesEvent";
+
+// Import constants.
+import { EVENT_NAME_CONNECT_NODE } from "../../constants/EventNames";
+
+// Import context.
+import { useEventBus } from "../../context/EventBusContext";
+
+/**
+ * React hook to dispatch ConnectNodesEvent using the shared event bus.
+ * Returns a memoized function.
+ */
+export const useConnectNode = () => {
+	const eventBus = useEventBus();
+	return useCallback(
+		(event: ConnectNodesEvent) => {
+			const customEvent = new CustomEvent(EVENT_NAME_CONNECT_NODE, {
+				detail: event,
+			});
+			eventBus.dispatchEvent(customEvent);
+		},
+		[eventBus],
+	);
+};
